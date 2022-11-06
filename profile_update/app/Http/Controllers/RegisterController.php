b62f778a853237471b9fa97c2a903d462dcca699
AlamMorium7 <110713699+AlamMorium7@users.noreply.github.com>
2022-11-06 19:18:55 +0600

    Update RegisterController.php

--- profile_update/app/Http/Controllers/RegisterController.php
+++ profile_update/app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\DB;
+use App\Http\Requests\StoreRegistration;
 
 
 class RegisterController extends Controller
@@ -42,6 +43,61 @@ class RegisterController extends Controller
 
            ]);
 
-         
+         <!--new-->
+              public function GetLoginUser(Request $request){
+        $user = RegistrationUserModel::where("email",$request->email)->first();
+        if($user){
+            if(password_verify($request->password,$user->password)){
+                //save details in session
+                $request->session()->put("user",$user);
+                return redirect()->route("dashboard");
+            }
+            else{
+                dd("Password Wrong");
+            }
+        }
+        else{
+            dd("User Not Found");
+        }
+    }
+
+    public function GetLoginUserPage(){
+        return view("login");
+    }
+
+    public function LogoutUser(Request $request){
+        $request->session()->forget("user");
+        return redirect()->route("index");
+    }
+
+    public function GetDashboard(Request $request){
+        if($request->session()->has("user")){
+            return view("dashboard");
+        }
+        else{
+            return redirect()->route("index");
+        }
+    }
+
+    public function EditProfile(Request $request){
+        if($request->session()->has("user")){
+            return view("editprofile");
+        }
+        else{
+            return redirect()->route("index");
+        }
+    }
+
+    public function UpdateProfile(Request $request){
+        $user = RegistrationUserModel::find($request->id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->age = $request->age;
+        if($user->save()){
+            $request->session()->put("user",$user);
+            return redirect()->route("dashboard");
+        }
+    }
+      
    }
 }
